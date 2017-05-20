0839428076ed23ceebbef9023bd031f0594e81e6
Mark Sturzbecher <mark.sturzbecher@gmail.com>
2017-05-20 11:45:12 -0700

    Menu and LeveManager script

---
+++ Colloseum17_fantasy/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        LoadScene("", LoadSceneMode.Additive);
+	}
+
+    private static void LoadScene(string v, object additive)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static SceneManagement.Scene GetSceneByName(string name)
+    {
+        
+    }
+
+    public static bool SetActiveScene(SceneManagement.Scene scene)
+    {
+
+    }
+
+ 
