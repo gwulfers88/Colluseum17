36c9594947ad078a3d57668aeb79cbe7e6a7a5e8
WolfManJack805 <david_magana2@my.vcccd.edu>
2017-06-03 01:08:43 -0700

    Hello Brother!!

---
+++ Colloseum17_fantasy/Assets/Scripts/KillZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
